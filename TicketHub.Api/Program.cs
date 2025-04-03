b79dcfca0b16a82b2f871362e2b3ac34d1ff2f24
MhamadWalid <malnassan2005@gmail.com>
2025-04-03 13:18:21 -0300

    Ready

--- TicketHub.Api/Program.cs
+++ TicketHub.Api/Program.cs
@@ -1,27 +1,19 @@
-using Microsoft.OpenApi.Models;
-using TicketHub.Api.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // This is needed for Swagger
-builder.Services.AddSwaggerGen();           // This registers Swagger
-builder.Services.AddSingleton<QueueService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();         // This generates the Swagger JSON
-    app.UseSwaggerUI();       // This generates the Swagger UI
-}
+// Enable Swagger ALWAYS (even in production)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
